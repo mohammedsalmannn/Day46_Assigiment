7635213d9ec751e6a3f0168e486391840287a70f
mohammedsalmannn <rmdsalman001@gmail.com>
2021-07-13 13:58:51 +0530

    Added : adding events to from using javaScript file

---
+++ js/payroll_from.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector("#name");
+    const nameError = document.querySelector(".name-error");
+    name.addEventListener("input", function () {
+        if (name.value.length == 0) {
+            nameError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            nameError.textContent = "";
+        } catch (error) {
+            nameError.textContent = error;
+        }
+    });
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector(".salary-output");
+    output.textContent = salary.value;
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
+    });
+
+});
