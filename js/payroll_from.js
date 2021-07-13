8f39e819d5584874785dca77ded0e7cbd472503c
mohammedsalmannn <rmdsalman001@gmail.com>
2021-07-13 14:04:47 +0530

    Added : adding employee Object using js file

--- js/payroll_from.js
+++ js/payroll_from.js
@@ -21,3 +21,73 @@ window.addEventListener('DOMContentLoaded', (event) => {
     });
 
 });
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector("#name");
+    const nameError = document.querySelector(".name-error");
+    name.addEventListener("input", function () {
+        if (name.value.length == 0) {
+            nameError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            nameError.textContent = "";
+        } catch (error) {
+            nameError.textContent = error;
+        }
+    });
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector(".salary-output");
+    output.textContent = salary.value;
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
+    });
+
+});
+
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+    } catch (error) {
+        return;
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try {
+        employeePayrollData.name = getInputValueById("#name");
+    } catch (error) {
+        setTextValue('.name-error', error);
+        throw error;
+    }
+    employeePayrollData.profilePic = getSelectedValues("[name = profile]").pop();
+    employeePayrollData.gender = getSelectedValues("[name = gender]").pop();
+    employeePayrollData.department = getSelectedValues("[name = department]");
+    employeePayrollData.salary = getInputValueById("#salary");
+    employeePayrollData.notes = getInputValueById("#notes");
+    employeePayrollData.startDate = new Date(getInputValueById("#year"), getInputValueById("#month"), getInputValueById("#day"));
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+
+const getSelectedValues = (property) => {
+    let allItems = document.querySelectorAll(property);
+    let selectedItems = [];
+    allItems.forEach(item => {
+        if (item.checked)
+            selectedItems.push(item.value);
+    });
+    return selectedItems;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
